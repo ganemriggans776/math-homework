fae8ca7284b427f6cc0e3ed85d78be8a57622acc
ganemriggans776 <ganemriggans776@gmail.com>
2025-03-24 22:27:41 +0000

    Add 15.py

---
+++ src/15.py
@@ -0,0 +1,8 @@
+def calculate_area_triangle(base, height):
+    area = 0.5 * base * height
+    return area
+
+base = 4
+height = 6
+area = calculate_area_triangle(base, height)
+print("The area of the triangle is:", area)
