bb3965ce57bf69a9d703aff061b00a43cf2caa35
ganemriggans776 <ganemriggans776@gmail.com>
2025-05-19 06:55:10 +0000

    Add 36.py

---
+++ src/36.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_square_root(num):
+    """
+    Calculate the square root of a given number.
+    
+    Args:
+        num (float): The number to find the square root of.
+        
+    Returns:
+        float: The square root of the input number.
+    """
+    return math.sqrt(num)
+
+# Example usage
+result = calculate_square_root(16)
+print(result)  # Output should be 4.0
